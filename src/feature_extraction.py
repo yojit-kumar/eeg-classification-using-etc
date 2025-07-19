18a0fd6cb4edd60f43668113b6d351d0d0d16b68
yojit-kumar <yojit.kumar@students.iiserpune.ac.in>
2025-07-20 02:56:43 +0530

    minor fixes

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -3,34 +3,8 @@ import pandas as pd
 
 
 def etc_func(signal):
-    seq = ETC.partition(signal, n_bins=4)
+    seq = ETC.partition(signal, n_bins=2)
     res = ETC.compute_1D(seq, verbose=False).get('NETC1D')
     return res
 
-def etc_with_no_order(r1, r2, volunteer_ids, channel_ids):
-    results=[]
-    for v in vounteer_ids:
-        labels = r1.ch_names
-        label_idx = {label: idx for idx, label in enumerate(labels)}
-
-        r1_data = r1.get_data()
-        r2_data = r2.get_data()
-
-        for ch in channel_ids:
-            idx = label_idx[ch]
-            signal1 = r1_data[idx,:]
-            signal2 = r2_data[idx,:]
-
-            etc_1 = etc_func(signal1)
-            etc_2 = etc_func(signal2)
-
-            results.append({
-                'volunteer': v,
-                'channel': ch,
-                'ETC_EyesOpen': etc_1,
-                'ETC_EyesClosed': etc_2,
-                })
-            
-        df = pd.DataFrame(results)
-    return df
     
