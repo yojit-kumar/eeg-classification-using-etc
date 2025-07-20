8c92a40367b6a4da8221845adedfc7940a7abbc5
yojit-kumar <yojit.kumar@students.iiserpune.ac.in>
2025-07-20 15:31:19 +0530

    added decision_tree

--- src/ml_model.py
+++ src/ml_model.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import KFold
 
 
 from sklearn.ensemble import AdaBoostClassifier
-
+from sklearn.tree import DecisionTreeClassifier
 
 def data_split(dataframe):
     volunteers = dataframe['volunteer'].unique()
@@ -75,7 +75,7 @@ def adaboost_train(X_train, y_train):
     try:
         os.makedirs(RESULT_PATH)
     except OSError:
-        print("Creation of result dir not rrequired")
+        print("Creation of result dir not required")
 
     np.save(RESULT_PATH+"NEST.npy", np.array([BESTNEST]))
     np.save(RESULT_PATH+"F1SCORE.npy", np.array([BESTF1]))
@@ -114,6 +114,88 @@ def adaboost_test(X_train, y_train, X_test, y_test):
     np.save(RESULT_PATH+"/RECALL_TEST.npy", np.array([recall]) )
 
     
+def decisiontree_train(X_train, y_train):
+    msl_values = [2, 5, 10, 15]
+    md_values = [2, 3, 4, 5]
+
+    BESTF1 = 0
+    FOLD_NO = 5
+
+    KF = KFold(n_splits=FOLD_NO, random_state=42, shuffle=True)
+    KF.get_n_splits(X_train)
+
+    clf = DecisionTreeClassifier(random_state=42)
+    ccp_path = clf.cost_complexity_pruning_path(X_train, y_train)
+    alpha = ccp_path['ccp_alphas']
+
+    for MSL in msl_values:
+        for MD in md_values:
+            for CCP in alpha:
+                FSCORE_TEMP = []
+                for train_idx, val_idx in KF.split(X_train):
+                    X_TRAIN, X_VAL = X_train[train_idx], X_train[val_idx]
+                    Y_TRAIN, Y_VAL = y_train[train_idx], y_train[val_idx]
+
+                    clf = DecisionTreeClassifier(min_samples_leaf=MSL, random_state=42, max_depth=MD, ccp_alpha=CCP)
+                    clf.fit(X_TRAIN, Y_TRAIN.ravel())
+                    Y_PRED = clf.predict(X_VAL)
+
+                    f1 = f1_score(Y_VAL, Y_PRED, average='macro')
+                    FSCORE_TEMP.append(f1)
+
+                MEAN_FSCORE_TEMP = np.mean(FSCORE_TEMP)
+                if MEAN_FSCORE_TEMP > BESTF1:
+                    BESTF1 = MEAN_FSCORE_TEMP
+                    BESTMSL = MSL
+                    BESTMD = MD
+                    BESTCCP = CCP
 
 
+    RESULT_PATH = '../results/parameters/decisiontree/'
 
+    try:
+        os.makedirs(RESULT_PATH)
+    except OSError:
+        print("Creation of result dir not required")
+
+    np.save(RESULT_PATH+"MSL.npy", np.array([BESTMSL]))
+    np.save(RESULT_PATH+"MD.npy", np.array([BESTMD]))
+    np.save(RESULT_PATH+"CCP.npy", np.array([BESTCCP]))
+    np.save(RESULT_PATH+"F1SCORE.npy", np.array([BESTF1]))
+
+    print("Training Finished!")
+
+
+def decisiontree_test(X_train, y_train, X_test, y_test):
+    RESULT_PATH = '../results/parameters/decisiontree/'
+
+    MSL = np.load(RESULT_PATH+'MSL.npy')[0]
+    MD = np.load(RESULT_PATH+'MD.npy')[0]
+    CCP = np.load(RESULT_PATH+'CCP.npy')[0]
+    F1SCORE = np.load(RESULT_PATH+'F1SCORE.npy')[0]
+
+    clf = DecisionTreeClassifier(min_samples_leaf=MSL, random_state=42, max_depth=MD, ccp_alpha=CCP)
+    clf.fit(X_train, y_train.ravel())
+    
+    Y_TEST = y_test
+    Y_PRED = clf.predict(X_test)
+
+    acc = accuracy_score(Y_TEST, Y_PRED)
+    f1 = f1_score(Y_TEST, Y_PRED, average='macro')
+    prec = precision_score(Y_TEST, Y_PRED, average='macro')
+    recall = recall_score(Y_TEST, Y_PRED, average='macro')
+
+
+    print('TRAINING F1 Score', F1SCORE)
+
+    print('ACCURACY', acc)
+    print('TESTING F1 Score', f1)
+    print('PRECISION', prec)
+    print('RECALL', recall)
+
+    np.save(RESULT_PATH+"/F1SCORE_TEST.npy", np.array([f1]) )
+    np.save(RESULT_PATH+"/ACCURACY_TEST.npy", np.array([acc]) )
+    np.save(RESULT_PATH+"/PRECISION_TEST.npy", np.array([prec]) )
+    np.save(RESULT_PATH+"/RECALL_TEST.npy", np.array([recall]) )
+
+                    
